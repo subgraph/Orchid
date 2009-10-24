85f33d22c400ae475eedd6952568995b0fcd3a7b
Brian O'Blivion <bob@brian-oblivions-macbook.local>
2009-10-24 12:52:44 -0400

    Added circuit support

--- src/org/torproject/jtor/Tor.java
+++ src/org/torproject/jtor/Tor.java
@@ -2,9 +2,14 @@ package org.torproject.jtor;
 
 
 import java.security.Security;
+import java.util.List;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.torproject.jtor.circuits.Circuit;
+import org.torproject.jtor.circuits.impl.CircuitImpl;
+import org.torproject.jtor.circuits.impl.ConnectionManagerImpl;
 import org.torproject.jtor.directory.Directory;
+import org.torproject.jtor.directory.RouterDescriptor;
 import org.torproject.jtor.directory.impl.DirectoryImpl;
 import org.torproject.jtor.directory.impl.DocumentParserFactoryImpl;
 import org.torproject.jtor.directory.parsing.DocumentParserFactory;
@@ -12,6 +17,7 @@ import org.torproject.jtor.directory.parsing.DocumentParserFactory;
 public class Tor {
 	private final Directory directory;
 	private final DocumentParserFactory parserFactory;
+	private final ConnectionManagerImpl linkManager;
 	private final Logger logger;
 	
 	public Tor() {
@@ -23,6 +29,15 @@ public class Tor {
 		this.logger = logger;
 		this.directory = new DirectoryImpl(logger);
 		parserFactory = new DocumentParserFactoryImpl(logger);
+		linkManager = new ConnectionManagerImpl();
+	}
+	
+	public Circuit createCircuitFromNicknames(List<String> nicknamePath) {
+		final List<RouterDescriptor> path = directory.getRouterListByNames(nicknamePath);
+		return createCircuit(path);
+	}
+	public Circuit createCircuit(List<RouterDescriptor> path) {
+		return CircuitImpl.create(linkManager, path);
 	}
 	
 	public Logger getLogger() {
@@ -37,4 +52,7 @@ public class Tor {
 		return parserFactory;
 	}
 
+	public ConnectionManagerImpl getLinkManager() {
+		return linkManager;
+	}
 }
