6a19b08e7a6ed619cbb8c93f5acfff9923a0a55d
Brian O'Blivion <bob@brian-oblivions-macbook.local>
2009-10-24 12:44:17 -0400

    added equals() and hashcode(). Made some methods public

--- src/org/torproject/jtor/directory/impl/router/RouterDescriptorImpl.java
+++ src/org/torproject/jtor/directory/impl/router/RouterDescriptorImpl.java
@@ -17,50 +17,50 @@ public class RouterDescriptorImpl implements RouterDescriptor {
 	private IPv4Address address;
 	private int routerPort;
 	private int directoryPort;
-	
+
 	private int averageBandwidth = -1;
 	private int burstBandwidth = -1;
 	private int observedBandwidth = -1;
-	
+
 	private String platform;
-	
+
 	private Timestamp published;
-	
+
 	private HexDigest fingerprint;
-	
+
 	private boolean hibernating;
-	
+
 	private int uptime;
-	
+
 	private TorPublicKey onionKey;
 	private TorPublicKey identityKey;
 	private ExitPolicy exitPolicy = new ExitPolicy();
-	
+
 	private String contact;
 	private Set<String> families = Collections.emptySet();
 	private Set<Integer> linkProtocols = Collections.emptySet();
 	private Set<Integer> circuitProtocols = Collections.emptySet();
-	
+
 	private BandwidthHistory readHistory;
 	private BandwidthHistory writeHistory;
-	
+
 	private boolean eventDNS = false;
 	private boolean cachesExtraInfo = false;
 	private boolean hiddenServiceDir = false;
 	private HexDigest extraInfoDigest = null;
 	private boolean allowSingleHopExits = false;
 	private boolean hasValidSignature = false;
-	
-	void setNickname(String nickname) { this.nickname = nickname; }
-	void setAddress(IPv4Address address) { this.address = address; }
-	void setRouterPort(int port) { this.routerPort = port; }
+
+	public void setNickname(String nickname) { this.nickname = nickname; }
+	public void setAddress(IPv4Address address) { this.address = address; }
+	public void setRouterPort(int port) { this.routerPort = port; }
 	void setDirectoryPort(int port) { this.directoryPort = port; }
 	void setPlatform(String platform) { this.platform = platform; }
 	void setPublished(Timestamp published) { this.published = published; }
 	void setFingerprint(HexDigest fingerprint) { this.fingerprint = fingerprint; }
 	void setHibernating(boolean flag) { this.hibernating = flag; }
 	void setUptime(int uptime) { this.uptime = uptime; }
-	void setOnionKey(TorPublicKey key) { this.onionKey = key; }
+	public void setOnionKey(TorPublicKey key) { this.onionKey = key; }
 	void setIdentityKey(TorPublicKey key) { this.identityKey = key; }
 	void setContact(String contact) { this.contact = contact; }
 	void setEventDNS() { eventDNS = true; }
@@ -71,114 +71,114 @@ public class RouterDescriptorImpl implements RouterDescriptor {
 	void setReadHistory(BandwidthHistory history) { this.readHistory= history; }
 	void setWriteHistory(BandwidthHistory history) { this.writeHistory = history; }
 	void setValidSignature() { hasValidSignature = true; }
-	
+
 	void addAcceptRule(String rule) {
 		exitPolicy.addAcceptRule(rule);
 	}
-	
+
 	void addRejectRule(String rule) {
 		exitPolicy.addRejectRule(rule);
 	}
-	
+
 	void setBandwidthValues(int average, int burst, int observed) {
 		this.averageBandwidth = average;
 		this.burstBandwidth = burst;
 		this.observedBandwidth = observed;
 	}
+
 	void addFamily(String family) {
 		if(families.isEmpty())
 			families = new HashSet<String>();
 		families.add(family);
 	}
-	
+
 	void addCircuitProtocolVersion(int version) {
 		if(circuitProtocols.isEmpty())
 			circuitProtocols = new HashSet<Integer>();
 		circuitProtocols.add(version);
 	}
-	
+
 	void addLinkProtocolVersion(int version) {
 		if(linkProtocols.isEmpty())
 			linkProtocols = new HashSet<Integer>();
 		linkProtocols.add(version);	
 	}
-	
-	
+
 	public boolean isValidDocument() {
 		// verify required fields exist, see dirspec.txt section 2.1
 		return hasValidSignature && (nickname != null) && (address != null) &&
 			(averageBandwidth != -1) && (routerPort != 0 || directoryPort != 0) &&
 			(published != null) && (onionKey != null) && (identityKey != null);
 	}
-	
+
 	public String getNickname() {
 		return nickname;
 	}
-	
+
 	public IPv4Address getAddress() {
 		return address;
 	}
-	
+
 	public int getRouterPort() {
 		return routerPort;
 	}
-	
+
 	public int getDirectoryPort() {
 		return directoryPort;
 	}
-	
+
 	public int getAverageBandwidth() {
 		return averageBandwidth;
 	}
-	
+
 	public int getBurstBandwidth() {
 		return burstBandwidth;
 	}
-	
+
 	public int getObservedBandwidth() {
 		return observedBandwidth;
 	}
-	
+
 	public String getPlatform() {
 		return platform;
 	}
-	
+
 	public HexDigest getFingerprint() {
 		return fingerprint;
 	}
-	
+
 	public int getUptime() {
 		return uptime;
 	}
-	
+
 	public TorPublicKey getOnionKey() {
 		return onionKey;
 	}
-	
+
 	public TorPublicKey getIdentityKey() {
 		return identityKey;
 	}
-	
+
 	public String getContact() {
 		return contact;
 	}
-	
+
 	public boolean isHibernating() {
 		return hibernating;
 	}
-	
+
 	public boolean cachesExtraInfo() {
 		return cachesExtraInfo;
 	}
-	
+
 	public boolean allowsSingleHopExits() {
 		return allowSingleHopExits;
 	}
-	
+
 	public Timestamp getPublishedTime() {
 		return published;
 	}
-	
+
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("Router Descriptor: (name: ");
@@ -222,16 +222,32 @@ public class RouterDescriptorImpl implements RouterDescriptor {
 	public boolean isInFamily(String family) {
 		return families.contains(family);
 	}
-	
+
 	public boolean supportsEventDNS() {
 		return eventDNS;
 	}
-	
+
 	public BandwidthHistory getReadHistory() {
 		return readHistory;
 	}
-	
+
 	public BandwidthHistory getWriteHistory() {
 		return writeHistory;
+	}
+
+	public boolean equals(Object o) {
+		if(!(o instanceof RouterDescriptorImpl)) 
+			return false;
+		final RouterDescriptorImpl other = (RouterDescriptorImpl) o;
+		if(other.getIdentityKey() == null || getIdentityKey() == null)
+			return false;
+
+		return other.getIdentityKey().equals(getIdentityKey());
+	}
+
+	public int hashCode() {
+		if(getIdentityKey() == null)
+			return 0;
+		return getIdentityKey().hashCode();
 	}
 }
