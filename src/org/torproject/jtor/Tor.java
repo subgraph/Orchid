0232e75646fd088b8d3d12ff9ad9e340ea8f9473
Brian O'Blivion <bob@brian-oblivions-macbook.local>
2009-11-05 10:15:37 -0500

    Link manager was renamed to connection manager

--- src/org/torproject/jtor/Tor.java
+++ src/org/torproject/jtor/Tor.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.torproject.jtor.circuits.Circuit;
-import org.torproject.jtor.circuits.impl.CircuitImpl;
+import org.torproject.jtor.circuits.impl.CircuitManagerImpl;
 import org.torproject.jtor.circuits.impl.ConnectionManagerImpl;
 import org.torproject.jtor.directory.Directory;
 import org.torproject.jtor.directory.RouterDescriptor;
@@ -17,7 +17,8 @@ import org.torproject.jtor.directory.parsing.DocumentParserFactory;
 public class Tor {
 	private final Directory directory;
 	private final DocumentParserFactory parserFactory;
-	private final ConnectionManagerImpl linkManager;
+	private final ConnectionManagerImpl connectionManager;
+	private final CircuitManagerImpl circuitManager;
 	private final Logger logger;
 	
 	public Tor() {
@@ -29,15 +30,17 @@ public class Tor {
 		this.logger = logger;
 		this.directory = new DirectoryImpl(logger);
 		parserFactory = new DocumentParserFactoryImpl(logger);
-		linkManager = new ConnectionManagerImpl();
+		connectionManager = new ConnectionManagerImpl();
+		circuitManager = new CircuitManagerImpl(directory, connectionManager);
 	}
 	
 	public Circuit createCircuitFromNicknames(List<String> nicknamePath) {
 		final List<RouterDescriptor> path = directory.getRouterListByNames(nicknamePath);
 		return createCircuit(path);
 	}
+	
 	public Circuit createCircuit(List<RouterDescriptor> path) {
-		return CircuitImpl.create(linkManager, path);
+		return circuitManager.createCircuitFromPath(path);
 	}
 	
 	public Logger getLogger() {
@@ -52,7 +55,7 @@ public class Tor {
 		return parserFactory;
 	}
 
-	public ConnectionManagerImpl getLinkManager() {
-		return linkManager;
+	public ConnectionManagerImpl getConnectionManager() {
+		return connectionManager;
 	}
 }
