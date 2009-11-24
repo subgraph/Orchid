1de3baf3895f0b742565fcb29379dda63ddf8a74
Brian O'Blivion <bob@brian-oblivions-macbook.local>
2009-11-24 10:29:24 -0500

    Add network status manager and config to Tor

--- src/org/torproject/jtor/Tor.java
+++ src/org/torproject/jtor/Tor.java
@@ -8,10 +8,12 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.torproject.jtor.circuits.Circuit;
 import org.torproject.jtor.circuits.impl.CircuitManagerImpl;
 import org.torproject.jtor.circuits.impl.ConnectionManagerImpl;
+import org.torproject.jtor.config.impl.TorConfigImpl;
 import org.torproject.jtor.directory.Directory;
-import org.torproject.jtor.directory.RouterDescriptor;
+import org.torproject.jtor.directory.Router;
 import org.torproject.jtor.directory.impl.DirectoryImpl;
 import org.torproject.jtor.directory.impl.DocumentParserFactoryImpl;
+import org.torproject.jtor.directory.impl.NetworkStatusManager;
 import org.torproject.jtor.directory.parsing.DocumentParserFactory;
 
 public class Tor {
@@ -20,6 +22,8 @@ public class Tor {
 	private final ConnectionManagerImpl connectionManager;
 	private final CircuitManagerImpl circuitManager;
 	private final Logger logger;
+	private final TorConfig config;
+	private final NetworkStatusManager statusManager;
 	
 	public Tor() {
 		this(new ConsoleLogger());
@@ -28,18 +32,26 @@ public class Tor {
 	public Tor(Logger logger) {
 		Security.addProvider(new BouncyCastleProvider());
 		this.logger = logger;
-		this.directory = new DirectoryImpl(logger);
+		this.config = new TorConfigImpl();
+		this.directory = new DirectoryImpl(logger, config);
 		parserFactory = new DocumentParserFactoryImpl(logger);
 		connectionManager = new ConnectionManagerImpl();
 		circuitManager = new CircuitManagerImpl(directory, connectionManager);
+		statusManager = new NetworkStatusManager(directory, logger);
+	}
+	
+	
+	public void start() {
+		directory.loadFromStore();
+		statusManager.startDownloadingDocuments();
 	}
 	
 	public Circuit createCircuitFromNicknames(List<String> nicknamePath) {
-		final List<RouterDescriptor> path = directory.getRouterListByNames(nicknamePath);
+		final List<Router> path = directory.getRouterListByNames(nicknamePath);
 		return createCircuit(path);
 	}
 	
-	public Circuit createCircuit(List<RouterDescriptor> path) {
+	public Circuit createCircuit(List<Router> path) {
 		return circuitManager.createCircuitFromPath(path);
 	}
 	
@@ -51,6 +63,10 @@ public class Tor {
 		return directory;
 	}
 	
+	public TorConfig getConfig() {
+		return config;
+	}
+	
 	public DocumentParserFactory getDocumentParserFactory() {
 		return parserFactory;
 	}
@@ -58,4 +74,5 @@ public class Tor {
 	public ConnectionManagerImpl getConnectionManager() {
 		return connectionManager;
 	}
+	
 }
