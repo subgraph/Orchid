79acea4c483ce8465fae17cf4d08d9cd95eaa283
Brian O'Blivion <bob@brian-oblivions-macbook.local>
2009-11-24 10:25:13 -0500

    API changed for parsing documents

--- src/org/torproject/jtor/directory/parsing/DocumentParser.java
+++ src/org/torproject/jtor/directory/parsing/DocumentParser.java
@@ -1,11 +1,6 @@
 package org.torproject.jtor.directory.parsing;
 
-import java.util.List;
-
-import org.torproject.jtor.directory.Directory;
 
 public interface DocumentParser<T> {
-	void parse();
-	void parseAndAddToDirectory(Directory directory);
-	List<T> getDocuments();
+	boolean parse(DocumentParsingResultHandler<T> resultHandler);
 }
