05effae8d5b024be5971fff7e03d473fcb1dad0c
Brian O'Blivion <bob@brian-oblivions-macbook.local>
2009-12-14 13:20:29 -0500

    Various methods added to Router

--- src/org/torproject/jtor/directory/Router.java
+++ src/org/torproject/jtor/directory/Router.java
@@ -12,7 +12,25 @@ public interface Router {
 	int getDirectoryPort();
 	TorPublicKey getIdentityKey();
 	HexDigest getIdentityHash();
+	boolean isDescriptorDownloadable();
+
 	RouterDescriptor getCurrentDescriptor();
 	HexDigest getDescriptorDigest();
 	TorPublicKey getOnionKey();
+	int getEstimatedBandwidth();
+	int getMeasuredBandwidth();
+
+	int getAverageBandwidth();
+	int getBurstBandwidth();
+	int getObservedBandwidth();
+	boolean isHibernating();
+	boolean isRunning();
+	boolean isValid();
+	boolean isBadExit();
+	boolean isPossibleGuard();
+	boolean isExit();
+	boolean isFast();
+	boolean isStable();
+	boolean exitPolicyAccepts(IPv4Address address, int port);
+	boolean exitPolicyAccepts(int port);
 }
--- src/org/torproject/jtor/directory/impl/RouterImpl.java
+++ src/org/torproject/jtor/directory/impl/RouterImpl.java
@@ -18,22 +18,22 @@ public class RouterImpl implements Router {
 	private final HexDigest identityHash;
 	protected RouterStatus status;
 	private RouterDescriptor descriptor;
-	
+
 	protected RouterImpl(RouterStatus status) {
 		identityHash = status.getIdentity();
 		this.status = status;
 	}
-	
+
 	void updateStatus(RouterStatus status) {
 		if(!identityHash.equals(status.getIdentity()))
 			throw new TorException("Identity hash does not match status update");
 		this.status = status;
 	}
-	
+
 	void updateDescriptor(RouterDescriptor descriptor) {
 		this.descriptor = descriptor;
 	}
-	
+
 	public boolean isDescriptorDownloadable() {
 		
 		if(descriptor != null && descriptor.getDescriptorDigest().equals(status.getDescriptorDigest()))
@@ -47,7 +47,7 @@ public class RouterImpl implements Router {
 	public HexDigest getDescriptorDigest() {
 		return status.getDescriptorDigest();
 	}
-	
+
 	public IPv4Address getAddress() {
 		return status.getAddress();
 	}
@@ -59,7 +59,41 @@ public class RouterImpl implements Router {
 	public boolean hasFlag(String flag) {
 		return status.hasFlag(flag);
 	}
-	
+
+	public boolean isHibernating() {
+		if(descriptor == null)
+			return false;
+		return descriptor.isHibernating();
+	}
+
+	public boolean isRunning() {
+		return hasFlag("Running");
+	}
+
+	public boolean isValid() {
+		return hasFlag("Valid");
+	}
+
+	public boolean isBadExit() {
+		return hasFlag("BadExit");
+	}
+
+	public boolean isPossibleGuard() {
+		return hasFlag("Guard");
+	}
+
+	public boolean isExit() {
+		return hasFlag("Exit");
+	}
+
+	public boolean isFast() {
+		return hasFlag("Fast");
+	}
+
+	public boolean isStable() {
+		return hasFlag("Stable");
+	}
+
 	public int getDirectoryPort() {
 		return status.getDirectoryPort();
 	}
@@ -79,9 +113,48 @@ public class RouterImpl implements Router {
 	public int getOnionPort() {
 		return status.getRouterPort();
 	}
-	
+
 	public TorPublicKey getOnionKey() {
 		return descriptor.getOnionKey();
 	}
 
+	public int getEstimatedBandwidth() {
+		return status.getEstimatedBandwidth();
+	}
+
+	public int getMeasuredBandwidth() {
+		return status.getMeasuredBandwidth();
+	}
+
+	public int getAverageBandwidth() {
+		if(descriptor == null)
+			return 0;
+		return descriptor.getAverageBandwidth();
+	}
+
+	public int getBurstBandwidth() {
+		if(descriptor == null)
+			return 0;
+		return descriptor.getBurstBandwidth();
+	}
+
+	public int getObservedBandwidth() {
+		if(descriptor == null)
+			return 0;
+		return descriptor.getObservedBandwidth();
+	}
+
+	public boolean exitPolicyAccepts(IPv4Address address, int port) {
+		if(descriptor == null)
+			return false;
+
+		if(address == null)
+			return descriptor.exitPolicyAccepts(port);
+
+		return descriptor.exitPolicyAccepts(address, port);
+	}
+
+	public boolean exitPolicyAccepts(int port) {
+		return exitPolicyAccepts(null, port);
+	}
 }
