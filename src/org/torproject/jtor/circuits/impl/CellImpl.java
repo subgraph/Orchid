08300046bfdf439d088c32e9ec1caa03bfe5cdb4
Brian O'Blivion <bob@brian-oblivions-macbook.local>
2009-12-22 12:07:14 -0500

    Nicer toString()

--- src/org/torproject/jtor/circuits/impl/CellImpl.java
+++ src/org/torproject/jtor/circuits/impl/CellImpl.java
@@ -11,11 +11,11 @@ class CellImpl implements Cell {
 	static CellImpl createCell(int circuitId, int command) {
 		return new CellImpl(circuitId, command);
 	}
-	
+
 	static CellImpl createVarCell(int circuitId, int command, int payloadLength) {
 		return new CellImpl(circuitId, command, payloadLength);
 	}
-	
+
 	static CellImpl readFromInputStream(InputStream input) throws IOException {
 		final ByteBuffer header = readHeaderFromInputStream(input);
 		final int circuitId = header.getShort() & 0xFFFF;
@@ -26,13 +26,13 @@ class CellImpl implements Cell {
 		readAll(input, cell.getCellBytes(), CELL_HEADER_LEN, CELL_PAYLOAD_LEN);
 		return cell;
 	}
-	
+
 	private static ByteBuffer readHeaderFromInputStream(InputStream input) throws IOException {
 		final byte[] cellHeader = new byte[CELL_HEADER_LEN];
 		readAll(input, cellHeader);
 		return ByteBuffer.wrap(cellHeader);
 	}
-	
+
 	private static CellImpl readVarCell(int circuitId, int command, InputStream input) throws IOException {
 		final byte[] lengthField = new byte[2];
 		readAll(input, lengthField);
@@ -41,11 +41,11 @@ class CellImpl implements Cell {
 		readAll(input, cell.getCellBytes(), CELL_VAR_HEADER_LEN, length);
 		return cell;
 	}
-	
+
 	private static void readAll(InputStream input, byte[] buffer) throws IOException {
 		readAll(input, buffer, 0, buffer.length);
 	}
-	
+
 	private static void readAll(InputStream input, byte[] buffer, int offset, int length) throws IOException {
 		int bytesRead = 0;
 		while(bytesRead < length) {
@@ -55,11 +55,11 @@ class CellImpl implements Cell {
 			bytesRead += n;
 		}
 	}
-	
+
 	private final int circuitId;
 	private final int command;
 	protected final ByteBuffer cellBuffer;
-	
+
 	/* Variable length cell constructor (ie: VERSIONS cells only) */
 	private CellImpl(int circuitId, int command, int payloadLength) {
 		this.circuitId = circuitId;
@@ -70,7 +70,7 @@ class CellImpl implements Cell {
 		cellBuffer.putShort((short) payloadLength);
 		cellBuffer.mark();
 	}
-	
+
 	/* Fixed length cell constructor */
 	protected CellImpl(int circuitId, int command) {
 		this.circuitId = circuitId;
@@ -80,7 +80,7 @@ class CellImpl implements Cell {
 		cellBuffer.put((byte) command);
 		cellBuffer.mark();
 	}
-	
+
 	protected CellImpl(byte[] rawCell) {
 		this.cellBuffer = ByteBuffer.wrap(rawCell);
 		this.circuitId = cellBuffer.getShort() & 0xFFFF;
@@ -91,67 +91,74 @@ class CellImpl implements Cell {
 	public int getCircuitId() {
 		return circuitId;
 	}
-	
+
 	public int getCommand() {
 		return command;
 	}
-	
+
 	public void resetToPayload() {
 		cellBuffer.reset();
 	}
-	
+
 	public int getByte() {
 		return cellBuffer.get() & 0xFF;
 	}
-	
+
 	public int getByteAt(int index) {
 		return cellBuffer.get(index) & 0xFF;
 	}
-	
+
 	public int getShort() {
 		return cellBuffer.getShort() & 0xFFFF;
 	}
-	
+
 	public int getInt() {
 		return cellBuffer.getInt();
 	}
-	
+
 	public int getShortAt(int index) {
 		return cellBuffer.getShort(index) & 0xFFFF;
 	}
-	
+
 	public void getByteArray(byte[] buffer) {
 		cellBuffer.get(buffer);
 	}
-	
+
 	public int cellBytesConsumed() {
 		return cellBuffer.position();
 	}
-	
+
 	public int cellBytesRemaining() {
 		return cellBuffer.remaining();
 	}
-	
+
 	public void putByte(int value) {
 		cellBuffer.put((byte) value);
 	}
-	
+
 	public void putByteAt(int index, int value) {
 		cellBuffer.put(index, (byte) value);
 	}
-	
+
 	public void putShort(int value) {
 		cellBuffer.putShort((short) value);
 	}
-	
+
 	public void putShortAt(int index, int value) {
 		cellBuffer.putShort(index, (short) value);
 	}
-	
+
 	public void putInt(int value) {
 		cellBuffer.putInt(value);
 	}
-	
+
+	public void putString(String string) {
+		final byte[] bytes = new byte[string.length() + 1];
+		for(int i = 0; i < string.length(); i++)
+			bytes[i] = (byte) string.charAt(i);
+		putByteArray(bytes);
+	}
+
 	public void putByteArray(byte[] data) {
 		cellBuffer.put(data);
 	}
@@ -159,13 +166,45 @@ class CellImpl implements Cell {
 	public void putByteArray(byte[] data, int offset, int length) {
 		cellBuffer.put(data, offset, length);
 	}
-	
+
 	public byte[] getCellBytes() {
 		return cellBuffer.array();
 	}
-	
+
 	public String toString() {
 		return "Cell: circuit_id="+ circuitId +" command="+ command +" payload_len="+ cellBuffer.position();
 	}
 
+	public static String errorToDescription(int errorCode) {
+		switch(errorCode) {
+		case ERROR_NONE:
+			return "No error reason given";
+		case ERROR_PROTOCOL:
+			return "Tor protocol violation";
+		case ERROR_INTERNAL:
+			return "Internal error";
+		case ERROR_REQUESTED:
+			return "Response to a TRUNCATE command sent from client";
+		case ERROR_HIBERNATING:
+			return "Not currently operating; trying to save bandwidth.";
+		case ERROR_RESOURCELIMIT:
+			return "Out of memory, sockets, or circuit IDs.";
+		case ERROR_CONNECTFAILED:
+			return "Unable to reach server.";
+		case ERROR_OR_IDENTITY:
+			return "Connected to server, but its OR identity was not as expected.";
+		case ERROR_OR_CONN_CLOSED:
+			return "The OR connection that was carrying this circuit died.";
+		case ERROR_FINISHED:
+			return "The circuit has expired for being dirty or old.";
+		case ERROR_TIMEOUT:
+			return "Circuit construction took too long.";
+		case ERROR_DESTROYED:
+			return "The circuit was destroyed without client TRUNCATE";
+		case ERROR_NOSUCHSERVICE:
+			return "Request for unknown hidden service";
+		default:
+			return "Error code "+ errorCode;
+		}
+	}
 }
--- src/org/torproject/jtor/circuits/impl/RelayCellImpl.java
+++ src/org/torproject/jtor/circuits/impl/RelayCellImpl.java
@@ -8,17 +8,18 @@ import org.torproject.jtor.circuits.cells.Cell;
 import org.torproject.jtor.circuits.cells.RelayCell;
 
 public class RelayCellImpl extends CellImpl implements RelayCell {
-	
+
 	public static RelayCell createFromCell(CircuitNode node, Cell cell) {
 		if(cell.getCommand() != Cell.RELAY)
 			throw new TorException("Attempted to create RelayCell from Cell type: "+ cell.getCommand());
 		return new RelayCellImpl(node, cell.getCellBytes());
 	}
-	
+
 	private final int streamId;
 	private final int relayCommand;
 	private final CircuitNode circuitNode;
-	
+	private final boolean isOutgoing;
+
 	/*
 	 * The payload of each unencrypted RELAY cell consists of:
      *     Relay command           [1 byte]
@@ -33,19 +34,21 @@ public class RelayCellImpl extends CellImpl implements RelayCell {
 		this.circuitNode = node;
 		this.relayCommand = relayCommand;
 		this.streamId = stream;
+		this.isOutgoing = true;
 		putByte(relayCommand);	// Command
 		putShort(0);			// 'Recognized'
 		putShort(stream);		// Stream
 		putInt(0);				// Digest
 		putShort(0);			// Length	
 	}
-	 
+
 	private RelayCellImpl(CircuitNode node, byte[] rawCell) {
 		super(rawCell);
 		this.circuitNode = node;
 		this.relayCommand = getByte();
 		getShort();
 		this.streamId = getShort();
+		this.isOutgoing = false;
 		getInt();
 		int payloadLength = getShort();
 		cellBuffer.mark(); // End of header
@@ -53,33 +56,116 @@ public class RelayCellImpl extends CellImpl implements RelayCell {
 			throw new TorException("Header length field exceeds total size of cell");
 		cellBuffer.limit(RelayCell.HEADER_SIZE + payloadLength);
 	}
-	
+
 	public int getStreamId() {
 		return streamId;
 	}
-	
+
 	public int getRelayCommand() {
 		return relayCommand;
 	}
-	
+
 	public void setLength() {
 		putShortAt(LENGTH_OFFSET, (short) (cellBytesConsumed() - HEADER_SIZE));
 	}
-	
+
 	public void setDigest(byte[] digest) {
 		for(int i = 0; i < 4; i++)
 			putByteAt(DIGEST_OFFSET + i, digest[i]);
 	}
-	
+
 	public ByteBuffer getPayloadBuffer() {
 		final ByteBuffer dup = cellBuffer.duplicate();
 		dup.reset();
 		return dup.slice();
 	}
-	
+
 	public CircuitNode getCircuitNode() {
 		return circuitNode;
 	}
 
+	public String toString() {
+		if(isOutgoing)
+			return "["+ commandToDescription(relayCommand) +" stream="+ streamId +" payload_len="+ (cellBytesConsumed() - HEADER_SIZE) +" dest="+ circuitNode +"]";
+		else
+			return "["+ commandToString() + " stream="+ streamId + " payload_len="+ cellBuffer.remaining() +" source="+ circuitNode + "]";
+	}
 
+	public String commandToString() {
+		if(relayCommand == RELAY_TRUNCATED) {
+			final int code = getByteAt(HEADER_SIZE);
+			return commandToDescription(relayCommand) + " ("+ CellImpl.errorToDescription(code) +")";
+		} else if(relayCommand == RELAY_END) {
+			final int code = getByteAt(HEADER_SIZE);
+			return commandToDescription(relayCommand) +" ("+ reasonToDescription(code) +")";
+		}
+		else
+			return commandToDescription(relayCommand);
+	}
+
+	public static String reasonToDescription(int reasonCode) {
+		switch(reasonCode) {
+		case REASON_MISC:
+			return "Unlisted reason";
+		case REASON_RESOLVEFAILED:
+			return "Couldn't look up hostname";
+		case REASON_CONNECTREFUSED:
+			return "Remote host refused connection";
+		case REASON_EXITPOLICY:
+			return "OR refuses to connect to host or port";
+		case REASON_DESTROY:
+			return "Circuit is being destroyed";
+		case REASON_DONE:
+			return "Anonymized TCP connection was closed";
+		case REASON_TIMEOUT:
+			return "Connection timed out, or OR timed out while connecting";
+		case REASON_HIBERNATING:
+			return "OR is temporarily hibernating";
+		case REASON_INTERNAL:
+			return "Internal error at the OR";
+		case REASON_RESOURCELIMIT:
+			return "OR has no resources to fulfill request";
+		case REASON_CONNRESET:
+			return "Connection was unexpectedly reset";
+		case REASON_TORPROTOCOL:
+			return "Tor protocol violation";
+		case REASON_NOTDIRECTORY:
+			return "Client sent RELAY_BEGIN_DIR to a non-directory server.";
+		default:
+			return "Reason code "+ reasonCode;
+		}
+	}
+
+	public static String commandToDescription(int command) {
+		switch(command) {
+		case RELAY_BEGIN:
+			return "RELAY_BEGIN";
+		case RELAY_DATA:
+			return "RELAY_DATA";
+		case RELAY_END:
+			return "RELAY_END";
+		case RELAY_CONNECTED:
+			return "RELAY_CONNECTED";
+		case RELAY_SENDME:
+			return "RELAY_SENDME";
+		case RELAY_EXTEND:
+			return "RELAY_EXTEND";
+		case RELAY_EXTENDED:
+			return "RELAY_EXTENDED";
+		case RELAY_TRUNCATE:
+			return "RELAY_TRUNCATE";
+		case RELAY_TRUNCATED:
+			return "RELAY_TRUNCATED";
+		case RELAY_DROP:
+			return "RELAY_DROP";
+		case RELAY_RESOLVE:
+			return "RELAY_RESOLVE";
+		case RELAY_RESOLVED:
+			return "RELAY_RESOLVED";
+		case RELAY_BEGIN_DIR:
+			return "RELAY_BEGIN_DIR";
+		default:
+			return "Relay command = "+ command;
+		}
+	}
 }
