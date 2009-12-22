8b049a840743ebacc3557ad4a8866053f57cef13
Brian O'Blivion <bob@brian-oblivions-macbook.local>
2009-12-22 12:01:12 -0500

    This is the correct interface for streams

--- src/org/torproject/jtor/circuits/Stream.java
+++ src/org/torproject/jtor/circuits/Stream.java
@@ -1,11 +1,9 @@
 package org.torproject.jtor.circuits;
 
-import org.torproject.jtor.data.IPv4Address;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public interface Stream {
-	boolean isExitStream();
-	int getExitPort();
-	IPv4Address getExitAddress();
-	String getExitHost();
-
+	InputStream getInputStream();
+	OutputStream getOutputStream();
 }
