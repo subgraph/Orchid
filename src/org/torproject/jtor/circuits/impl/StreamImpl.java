08748f6726358be18d67680d84a7e780cc7af1c4
Brian O'Blivion <bob@brian-oblivions-macbook.local>
2009-12-22 12:27:44 -0500

    Implemented.

--- src/org/torproject/jtor/circuits/impl/StreamImpl.java
+++ src/org/torproject/jtor/circuits/impl/StreamImpl.java
@@ -1,11 +1,13 @@
 package org.torproject.jtor.circuits.impl;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import org.torproject.jtor.TorException;
 import org.torproject.jtor.circuits.Circuit;
 import org.torproject.jtor.circuits.CircuitNode;
 import org.torproject.jtor.circuits.Stream;
 import org.torproject.jtor.circuits.cells.RelayCell;
-import org.torproject.jtor.data.IPv4Address;
 
 public class StreamImpl implements Stream {
 	private final CircuitImpl circuit;
@@ -13,6 +15,7 @@ public class StreamImpl implements Stream {
 	private final CircuitNode targetNode;
 	private final TorInputStream inputStream;
 	private final TorOutputStream outputStream;
+	private boolean isClosed;
 	
 	StreamImpl(CircuitImpl circuit, CircuitNode targetNode, int streamId) {
 		this.circuit = circuit;
@@ -21,56 +24,68 @@ public class StreamImpl implements Stream {
 		this.inputStream = new TorInputStream(this);
 		this.outputStream = new TorOutputStream(this);
 	}
-	
+
 	void addInputCell(RelayCell cell) {
-		inputStream.addInputCell(cell);
+		if(isClosed)
+			return;
+		if(cell.getRelayCommand() == RelayCell.RELAY_END)
+			inputStream.addEndCell(cell);
+		else
+			inputStream.addInputCell(cell);
 	}
-	
+
 	int getStreamId() {
 		return streamId;
 	}
-	
+
 	Circuit getCircuit() {
 		return circuit;
 	}
-	
+
 	CircuitNode getTargetNode() {
 		return targetNode;
 	}
-	
+
 	void close() {
+		if(isClosed)
+			return;
+		isClosed = true;
 		inputStream.close();
 		outputStream.close();
-		circuit.removeStream(this);
+		// XXX when to remove?
+		//circuit.removeStream(this);
 	}
-	
+
 	void openDirectory() {
 		final RelayCell cell = new RelayCellImpl(circuit.getFinalCircuitNode(), circuit.getCircuitId(), streamId, RelayCell.RELAY_BEGIN_DIR);
 		circuit.sendRelayCellToFinalNode(cell);
-		final RelayCell responseCell = circuit.receiveRelayResponse(RelayCell.RELAY_CONNECTED);
+		receiveRelayConnectedCell();
+	}
+
+	void openExit(String target, int port) {
+		final RelayCell cell = new RelayCellImpl(circuit.getFinalCircuitNode(), circuit.getCircuitId(), streamId, RelayCell.RELAY_BEGIN);
+		cell.putString(target + ":"+ port);
+		circuit.sendRelayCellToFinalNode(cell);
+		receiveRelayConnectedCell();
+	}
+
+	private RelayCell receiveRelayConnectedCell() {
+		final RelayCell responseCell = circuit.receiveRelayCell();
+		final int command = responseCell.getRelayCommand();
+		if(command != RelayCell.RELAY_CONNECTED)
+			throw new TorException("Did not receive expected RELAY_CONNECTED cell.  cell = "+ responseCell);
+
 		if(responseCell.getStreamId() != streamId) {
 			circuit.removeStream(this);
 			throw new TorException("Did not receive expected stream id");
-		}			
+		}
+		return responseCell;
+	}
+	public InputStream getInputStream() {
+		return inputStream;
 	}
 
-	public IPv4Address getExitAddress() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getExitHost() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public int getExitPort() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public boolean isExitStream() {
-		// TODO Auto-generated method stub
-		return false;
+	public OutputStream getOutputStream() {
+		return outputStream;
 	}
 }
