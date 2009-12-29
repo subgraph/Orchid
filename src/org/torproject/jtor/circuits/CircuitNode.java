29d162cc8f5bbe39ebfea9880e9618eff322ddae
Brian O'Blivion <bob@brian-oblivions-macbook.local>
2009-12-29 00:43:49 -0500

    Documentation for some of the public API interfaces.

--- src/org/torproject/jtor/circuits/CircuitNode.java
+++ src/org/torproject/jtor/circuits/CircuitNode.java
@@ -3,11 +3,48 @@ package org.torproject.jtor.circuits;
 import org.torproject.jtor.circuits.cells.RelayCell;
 import org.torproject.jtor.directory.Router;
 
+/**
+ * Represents the state of a single onion router hop in a connected or connecting {@link Circuit}
+ */
 public interface CircuitNode {
+	/**
+	 * Return the {@link Router} associated with this node.
+	 *
+	 * @return The {@link Router} for this hop of the circuit chain.
+	 */
 	Router getRouter();
-	void updateForwardDigest(RelayCell cell);
-	byte[] getForwardDigestBytes();
-	void encryptForwardCell(RelayCell cell);
-	CircuitNode getPreviousNode();
 
+	/**
+	 * Update the 'forward' cryptographic digest state for this
+	 * node with the contents of <code>cell</code>
+	 * 
+	 * @param cell The {@link RelayCell} to add to the digest.
+	 */
+	void updateForwardDigest(RelayCell cell);
+
+	/**
+	 * Return the current 'forward' running digest value for this 
+	 * node as an array of <code>TOR_DIGEST_SIZE</code> bytes.
+	 * 
+	 * @return The current 'forward' running digest value for this node.
+	 */
+	byte[] getForwardDigestBytes();
+
+	/**
+	 * Encrypt a {@link RelayCell} for this node with the current
+	 * 'forward' cipher state.
+	 * 
+	 * @param cell The {@link RelayCell} to encrypt.
+	 */
+	void encryptForwardCell(RelayCell cell);
+
+	/**
+	 * Return the {@link CircuitNode} which immediately preceeds this
+	 * one in the circuit node chain or <code>null</code> if this is
+	 * the first hop.
+	 * 
+	 * @return The previous {@link CircuitNode} in the chain or <code>
+	 *         null</code> if this is the first node.
+	 */
+	CircuitNode getPreviousNode();
 }
--- src/org/torproject/jtor/circuits/Connection.java
+++ src/org/torproject/jtor/circuits/Connection.java
@@ -4,15 +4,34 @@ import org.torproject.jtor.circuits.cells.Cell;
 import org.torproject.jtor.directory.Router;
 
 public interface Connection {
-	Router getRouter();
-	boolean isConnected();
-	void connect() throws ConnectionConnectException;
 	/**
+	 * Return the {@link Router} associated with this connection.
 	 * 
-	 * @param cell
-	 * @throws ConnectionClosedException
+	 * @return The entry router this connection represents.
+	 */
+	Router getRouter();
+
+	/**
+	 * Return <code>true</code> if this connection is currently connected.  Otherwise, <code>false</code>.
+	 * 
+	 * @return <code>true</code> if this connection is connected or <code>false</code> otherwise.
+	 */
+	boolean isConnected();
+
+	/**
+	 * If the network connection is not currently connected, attempt to open it.  If already connected
+	 * return immediately.
+	 * 
+	 * @throws ConnectionConnectException If an error occured while attempting to establish the connection.
+	 */
+	void connect() throws ConnectionConnectException;
+
+	/**
+	 * Send a protocol {@link Cell} on this connection.
+	 * 
+	 * @param cell The {@link Cell} to transfer.
+	 * @throws ConnectionClosedException If the cell could not be send because the connection is not connected
+	 *                                   or if an error occured while sending the cell data.
 	 */
 	void sendCell(Cell cell);
-	
-	
 }
