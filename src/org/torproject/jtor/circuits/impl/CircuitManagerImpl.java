28d519921448a99eb579f07cc96b31a5d72fe192
brl <bruce@netifera.com>
2010-08-15 15:13:28 -0400

    Remove unneeded import.

--- src/org/torproject/jtor/circuits/impl/CircuitManagerImpl.java
+++ src/org/torproject/jtor/circuits/impl/CircuitManagerImpl.java
@@ -1,7 +1,6 @@
 package org.torproject.jtor.circuits.impl;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
