c33b68dfd1ebee3ab0c3c8068e7986827b6ae88b
brl <bruce@netifera.com>
2010-08-15 00:17:01 -0400

    Reflect changes in OpenStreamResponse interface.

--- src/org/torproject/jtor/circuits/impl/OpenStreamResponseImpl.java
+++ src/org/torproject/jtor/circuits/impl/OpenStreamResponseImpl.java
@@ -6,19 +6,24 @@ import org.torproject.jtor.circuits.OpenStreamResponse;
 public class OpenStreamResponseImpl implements OpenStreamResponse {
 
 	static OpenStreamResponse createStreamOpened(Stream stream) {
-		return new OpenStreamResponseImpl(stream, OpenStreamStatus.STATUS_STREAM_OPENED);
+		return new OpenStreamResponseImpl(stream, OpenStreamStatus.STATUS_STREAM_OPENED, 0);
 	}
 	
-	static OpenStreamResponse createStreamError() {
-		return new OpenStreamResponseImpl(null, OpenStreamStatus.STATUS_ERROR_OTHER);
+	static OpenStreamResponse createStreamTimeout() {
+		return new OpenStreamResponseImpl(null, OpenStreamStatus.STATUS_STREAM_TIMEOUT, 0);
+	}
+	static OpenStreamResponse createStreamError(int reason) {
+		return new OpenStreamResponseImpl(null, OpenStreamStatus.STATUS_STREAM_ERROR, reason);
 	}
 	
+	private final int errorReason;
 	private final Stream stream;
 	private final OpenStreamStatus status;
 	
-	private OpenStreamResponseImpl(Stream stream, OpenStreamStatus status) {
+	private OpenStreamResponseImpl(Stream stream, OpenStreamStatus status, int reason) {
 		this.stream = stream;
 		this.status = status;
+		this.errorReason = reason;
 	}
 	
 	public Stream getStream() {
@@ -27,5 +32,13 @@ public class OpenStreamResponseImpl implements OpenStreamResponse {
 
 	public OpenStreamStatus getStatus() {
 		return status;
+	}
+
+	public int getErrorCode() {
+		return errorReason;
+	}
+
+	public String getErrorCodeMessage() {
+		return RelayCellImpl.reasonToDescription(errorReason);
 	}
 }
