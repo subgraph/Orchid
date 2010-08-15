37dab4cc1fea33c79aae016dd90f770bf479dcfd
brl <bruce@netifera.com>
2010-08-15 00:13:04 -0400

    Possible results from trying to open a stream are success, timeout, or an error as returned as the reason field in a RELAY_END cell.

--- src/org/torproject/jtor/circuits/OpenStreamResponse.java
+++ src/org/torproject/jtor/circuits/OpenStreamResponse.java
@@ -3,10 +3,18 @@ package org.torproject.jtor.circuits;
 public interface OpenStreamResponse {
 	enum OpenStreamStatus {
 		STATUS_STREAM_OPENED,
-		STATUS_ERROR_CONNECTION_REFUSED,
-		STATUS_ERROR_OTHER
+		STATUS_STREAM_TIMEOUT,
+		STATUS_STREAM_ERROR
 	};
 
 	OpenStreamStatus getStatus();
 	Stream getStream();
+	/**
+	 * The 'reason' code from a RELAY_END cell sent in response to a RELAY_BEGIN or RELAY_BEGIN_DIR
+	 * 
+	 * @return The error 'reason' from a RELAY_END cell.
+	 */
+	int getErrorCode();
+	
+	String getErrorCodeMessage();
 }
