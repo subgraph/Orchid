316df6b53079f02c81e435638fec0026c50378c8
brl <bruce@netifera.com>
2010-08-15 13:29:08 -0400

    Documentation for two new methods.

--- src/org/torproject/jtor/circuits/Circuit.java
+++ src/org/torproject/jtor/circuits/Circuit.java
@@ -123,6 +123,22 @@ public interface Circuit {
 	 */
 	CircuitNode getFinalCircuitNode();
 
+	/**
+	 * Return true if the final node of this circuit is believed to be able to connect to
+	 * the specified <code>ExitTarget</code>.  Returns false if the target destination is
+	 * not permitted by the exit policy of the final node in this circuit or if the target
+	 * has been previously recorded to have failed through this circuit.
+	 * 
+	 * @param target The exit destination.
+	 * @return Return true if is likely that the final node of this circuit can connect to the specified exit target.
+	 */
 	boolean canHandleExitTo(ExitTarget target);
+	
+	/**
+	 * Records the specified <code>ExitTarget</code> as a failed connection so that {@link #canHandleExitTo(ExitTarget)} will
+	 * no longer return true for this exit destination.
+	 * 
+	 * @param target The <code>ExitTarget</code> to which a connection has failed through this circuit.
+	 */
 	public void recordFailedExitTarget(ExitTarget target);
 }
