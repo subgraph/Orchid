14aa3db839ad4ea4861a64e85b8829d3cb8b8a42
brl <bruce@netifera.com>
2010-08-15 13:41:24 -0400

    Save the requested path in CircuitStatus when initiating build of a circuit.

--- src/org/torproject/jtor/circuits/impl/CircuitStatus.java
+++ src/org/torproject/jtor/circuits/impl/CircuitStatus.java
@@ -1,8 +1,12 @@
 package org.torproject.jtor.circuits.impl;
 
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
+import org.torproject.jtor.TorException;
 import org.torproject.jtor.crypto.TorRandom;
+import org.torproject.jtor.directory.Router;
 
 public class CircuitStatus {
 	private final static int MAXIMUM_TIMEOUT_COUNT = 3;
@@ -16,13 +20,14 @@ public class CircuitStatus {
 		CircuitState(String name) { this.name = name; }
 		public String toString() { return name; }
 	}
-	
+
 	private Date timestampCreated;
 	private Date timestampDirty;
 	private int streamTimeoutErrorCount = 0;
 	private int currentStreamId;
 	private Object streamIdLock = new Object();
 	private CircuitState state = CircuitState.UNCONNECTED;
+	private List<Router> circuitPath = Collections.emptyList();
 
 	CircuitStatus() {
 		initializeCurrentStreamId();
@@ -59,7 +64,7 @@ public class CircuitStatus {
 		if(then == null)
 			return 0;
 		final Date now = new Date();
-		return now.getTime() - then.getTime();	
+		return now.getTime() - then.getTime();
 	}
 
 	synchronized boolean isDirty() {
@@ -70,8 +75,21 @@ public class CircuitStatus {
 		state = CircuitState.OPEN;
 	}
 
-	void setStateBuilding() {
+	void setStateBuilding(List<Router> circuitPath) {
 		state = CircuitState.BUILDING;
+		this.circuitPath = Collections.unmodifiableList(circuitPath);
+	}
+
+	Router getFinalRouter() {
+		if(state == CircuitState.UNCONNECTED)
+			throw new TorException("Cannot retrieve last router from UNCONNECTED circuit");
+		if(circuitPath.size() == 0) 
+			throw new TorException("No routers on circuit (?!)");
+		return circuitPath.get(circuitPath.size() - 1);
+	}
+
+	List<Router> getCircuitPath() {
+		return circuitPath;
 	}
 
 	void setStateFailed() {
