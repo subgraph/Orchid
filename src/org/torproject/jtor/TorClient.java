4015ca76e5478937a8a47b1b236ce305684bb275
x <x@localhost.localdomain>
2013-03-05 13:07:10 -0500

    Some reorganizing to use this as the main library class

--- src/org/torproject/jtor/TorClient.java
+++ src/org/torproject/jtor/TorClient.java
@@ -4,6 +4,7 @@ import java.security.Security;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.torproject.jtor.circuits.CircuitManager;
+import org.torproject.jtor.circuits.OpenStreamResponse;
 import org.torproject.jtor.directory.Directory;
 import org.torproject.jtor.directory.impl.NetworkStatusManager;
 import org.torproject.jtor.logging.LogManager;
@@ -21,6 +22,8 @@ public class TorClient {
 	private final SocksPortListener socksListener;
 	private final NetworkStatusManager networkStatusManager;
 
+	private boolean isStarted = false;
+	
 	public TorClient() {
 		Security.addProvider(new BouncyCastleProvider());
 		logManager = Tor.createLogManager();
@@ -29,21 +32,44 @@ public class TorClient {
 		circuitManager = Tor.createCircuitManager(directory, logManager);
 		networkStatusManager = Tor.createNetworkStatusManager(directory, logManager);
 		socksListener = Tor.createSocksPortListener(logManager, circuitManager);
-		
+	}
+
+	public TorConfig getConfig() {
+		return config;
 	}
 
 	/**
 	 * Start running the Tor client service.
 	 */
-	public void start() {
+	public synchronized void start() {
+		if(isStarted) {
+			return;
+		}
+		
 		directory.loadFromStore();
 		networkStatusManager.startDownloadingDocuments();
 		circuitManager.startBuildingCircuits();
-		socksListener.addListeningPort(5090);
+		isStarted = true;
+	}
+
+	public OpenStreamResponse openExitStreamTo(String hostname, int port) throws InterruptedException {
+		if(!isStarted) {
+			throw new IllegalStateException("Must call start() first");
+		}
+		return circuitManager.openExitStreamTo(hostname, port);
+	}
+
+	public void enableSocksListener(int port) {
+		socksListener.addListeningPort(port);
+	}
+
+	public void enableSocksListener() {
+		enableSocksListener(5090);
 	}
 
 	public static void main(String[] args) {
 		final TorClient client = new TorClient();
 		client.start();
+		client.enableSocksListener();
 	}
 }
