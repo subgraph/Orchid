3fd8d1e382fd95382cd661c59b8bb08f6c7f08bf
x <x@localhost.localdomain>
2013-04-01 09:30:18 -0400

    Not used

--- src/org/torproject/jtor/control/KeyNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package org.torproject.jtor.control;
-
-public class KeyNotFoundException extends Exception {
-
-	private static final long serialVersionUID = 1L;
-
-	public KeyNotFoundException() {};
-}
