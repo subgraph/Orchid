648e8d19c4d30e3508ef9cf6e49ac95c82b3b63c
x <x@localhost.localdomain>
2013-04-07 15:12:11 -0400

    Rethrow ConnectionHandshakeException or else next catch clause will turn it into ConnectionFailedException

--- src/org/torproject/jtor/connections/ConnectionImpl.java
+++ src/org/torproject/jtor/connections/ConnectionImpl.java
@@ -119,6 +119,8 @@ public class ConnectionImpl implements Connection, DashboardRenderable {
 			} catch (InterruptedException e) {
 				Thread.currentThread().interrupt();
 				throw new ConnectionHandshakeException("Handshake interrupted");
+			} catch (ConnectionHandshakeException e) { 
+				throw e;
 			} catch (ConnectionIOException e) {
 				throw new ConnectionFailedException(e.getMessage());
 			}
