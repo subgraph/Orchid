8774d213e2977ec6e64eafc6e3daf93c30ab742c
brl <bruce@subgraph.com>
2013-07-09 08:51:34 -0400

    added method to extract DocumentObject content without header and footer lines.

--- src/com/subgraph/orchid/directory/parsing/DocumentObject.java
+++ src/com/subgraph/orchid/directory/parsing/DocumentObject.java
@@ -3,12 +3,17 @@ package com.subgraph.orchid.directory.parsing;
 public class DocumentObject {
 	
 	final private String keyword;
+	final private String headerLine;
+	private String footerLine;
+	private String bodyContent;
 	final private StringBuilder stringContent;
 	
-	public DocumentObject(String keyword) {
+	public DocumentObject(String keyword, String headerLine) {
 		this.keyword = keyword;
+		this.headerLine = headerLine;
 		this.stringContent = new StringBuilder();
 	}
+
 	public String getKeyword() {
 		return keyword;
 	}
@@ -18,8 +23,21 @@ public class DocumentObject {
 		stringContent.append("\n");
 	}
 	
+	public void addFooterLine(String footer) {
+		footerLine = footer;
+		bodyContent = stringContent.toString();
+	}
+	
 	public String getContent() {
-		return stringContent.toString();
+		return getContent(true);
+	}
+	
+	public String getContent(boolean includeHeaders) {
+		if(includeHeaders) {
+			return headerLine + "\n" + bodyContent + footerLine + "\n";
+		} else {
+			return bodyContent;
+		}
 	}
 
 }
