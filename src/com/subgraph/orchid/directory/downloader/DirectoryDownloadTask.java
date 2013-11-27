e76e7c12375eda1814c1a135e64f75ab658cdd7f
brl <bruce@subgraph.com>
2013-11-27 19:34:49 +0000

    Remove unused import

--- src/com/subgraph/orchid/directory/downloader/DirectoryDownloadTask.java
+++ src/com/subgraph/orchid/directory/downloader/DirectoryDownloadTask.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
